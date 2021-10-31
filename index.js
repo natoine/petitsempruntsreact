1ebf30fa303ba4bada2bf5ea1e6cc4ff74a8f716
natoine <antoineseilles@gmail.com>
2021-10-31 20:08:36 +0100

    no more var only const right now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000 ;
 
 app.get('/', function (req, res) {
