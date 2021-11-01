3691c1e854d08deae5301bc9fdc72b5b7e4919b2
natoine <antoineseilles@gmail.com>
2021-11-01 15:01:46 +0100

    install mongodb

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 
+const mongodb = require('mongodb');
+
 app.get('/', function (req, res) {
-  res.send('Bienvenue sur petits emprunts bientôt en react !');
+  res.send('Bienvenue sur petits emprunts bientôt en react - on bosse sur la home pour le moment !');
 });
 
 app.listen(PORT, function () {
