875bfc53c9051662f527fb5f492ce6f19bcc6312
natoine <antoineseilles@gmail.com>
2021-11-01 15:40:59 +0100

    sample test to post something on petitsemprunts db

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 3000 ;
 
 const mongodb = require('mongodb');
+const urimongo = require("./resources/secret/databaseconfig.js").url;
+console.log(urimongo);
 
 app.get('/', function (req, res) {
   res.send('Bienvenue sur petits emprunts bientôt en react - on bosse sur la home pour le moment !');
+});
+
+app.post('/newlend', function (req, res){
+  mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
+    let dbcol = client.db("petitsemprunts").collection("lends");
+    let myobj = {name: 'John', address: 'Highway 71'};
+    dbcol.insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      client.close();
+    });
+  });
 });
 
 app.listen(PORT, function () {
