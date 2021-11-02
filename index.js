84049a6e7d965a3bcd03551693d64809456887b4
natoine <antoineseilles@gmail.com>
2021-11-02 11:08:24 +0100

    now can parse a request body in json

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const mongodb = require('mongodb');
 const urimongo = require("./resources/secret/databaseconfig.js").url;
 //console.log(urimongo);
 
+app.use(express.json());//need to parse request body in json
+
 app.get('/', function (req, res) {
   res.send('Bienvenue sur petits emprunts bientôt en react - on bosse sur la home pour le moment !');
 });
 
 app.post('/newlend', function (request, response){
+  const newlend = request.body ;
+  console.log("lend",newlend);
   mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
     let dbcol = client.db("petitsemprunts").collection("lends");
     let myobj = {creator: 'John', loaner: 'Didier', borrower:'John', what:'BD thorgal - les archers', when:'now'};
